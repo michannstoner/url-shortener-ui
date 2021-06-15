eda945ea1651a4442300429e40d830d6e60d4e78
Your Name <stoner.michann@gmail.com>
2021-06-15 10:24:21 -0600

    Write POST method, pass as props to form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -19,15 +19,25 @@ export class App extends Component {
       .catch(error => this.setState({ error: 'Something went wrong, try again later!' }))
   }
 
+  shortenUrl = urlToShorten => {
+    fetch('http://localhost:3001/api/v1/urls', {
+      method: 'POST',
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify(urlToShorten)
+    })
+    .then(response => response.json())
+    .then(url => this.setState({ urls: [...this.state.urls, url]}))
+    .catch(error => this.setState({ error: 'We\'re experiencing issues, try later!'}))
+  }
+
   render() {
     console.log(this.state.urls);
     return (
       <main className="App">
         <header>
           <h1>URL Shortener</h1>
-          <UrlForm />
+          <UrlForm shortenUrl={this.shortenUrl}/>
         </header>
-
         <UrlContainer urls={this.state.urls}/>
       </main>
     );
