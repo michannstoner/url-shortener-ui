dbc7ff45d8646185eb38addde8ece5db685b4460
Your Name <stoner.michann@gmail.com>
2021-06-15 10:40:16 -0600

    Write additional tests for first it block

--- cypress/integration/url.spec.js
+++ cypress/integration/url.spec.js
@@ -6,5 +6,9 @@ describe('Url Shortener', () => {
 
     it('should display a page title and existing shortened URLs', () => {
       cy.get('h1').should('be.visible').should('contain', 'URL Shortener')
+        .get('.url').should('be.visible')
+          .get('h3').should('be.visible').should('contain', 'Awesome photo')
+          .get('a').should('be.visible').should('contain', 'http://localhost:3001/useshorturl/1')
+          .get('p').should('be.visible').should('contain', 'https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
     })
 })
