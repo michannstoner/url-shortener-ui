6e4e053f21d188e12b36f6a4ebd1b41dd60fda23
Your Name <stoner.michann@gmail.com>
2021-06-15 11:07:05 -0600

    Write describe and beforeEach for POST

--- cypress/integration/url.spec.js
+++ cypress/integration/url.spec.js
@@ -25,3 +25,21 @@ describe('Url Shortener', () => {
       .get('input[name="long_url"]').type('https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
   })
 })
+
+describe('POST requests', () => {
+  beforeEach(() => {
+    cy.intercept('http://localhost:3001/api/v1/urls', { fixture: 'test-data.json' })
+      .intercept({
+        method: 'POST',
+        url: 'http://localhost:3001/api/v1/urls'},
+      {
+        statusCode: 201,
+        body: {
+          'id': 1234567, 
+          'long_url': 'https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80', 
+          'short_url': 'http://localhost:3001/useshorturl/1', 'title': 'Title' 
+        } 
+      })
+      .visit('http://localhost:3000')
+  })
+})
