5017331f8b0d9ed0338cd13510128fa183a54030
Your Name <stoner.michann@gmail.com>
2021-06-15 10:48:36 -0600

    Write test for form display and button

--- cypress/integration/url.spec.js
+++ cypress/integration/url.spec.js
@@ -11,4 +11,11 @@ describe('Url Shortener', () => {
           .get('a').should('be.visible').should('contain', 'http://localhost:3001/useshorturl/1')
           .get('p').should('be.visible').should('contain', 'https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
     })
+
+    it('should display a form for a user to enter a URL to be shortened', () => {
+      cy.get('form').should('be.visible')
+        .get('input[name="title"]').should('be.empty').should('be.visible')
+        .get('input[name="long_url"]').should('be.empty').should('be.visible')
+        .get('button').should('be.visible').should('contain', 'Shorten Please!').should('be.disabled')
+    })
 })
