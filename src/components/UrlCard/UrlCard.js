7e64a2a4530cdde4e705c4965707a0d7c0ed4876
Your Name <stoner.michann@gmail.com>
2021-06-15 09:49:48 -0600

    Create UrlCard Component and return displaying props

---
+++ src/components/UrlCard/UrlCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const UrlCard = ( { title, short_url, long_url } ) => {
+  return (
+    <div className="url">
+        <h3>{title}</h3>
+        <a href={short_url} target="blank">{short_url}</a>
+        <p>{long_url}</p>
+      </div>
+  )
+}
+
+export default UrlCard
