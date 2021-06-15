d8735c3054498b71d961b73bda7e1179be1fa809
Your Name <stoner.michann@gmail.com>
2021-06-15 11:45:38 -0600

    Add error handling

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,12 @@
 export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
+      .then(checkForError)
+}
+
+const checkForError = response => {
+  if (!response.ok) {
+    return 'There was an error, try again later!'
+  } else {
+    return response.json()
+  }
 }
